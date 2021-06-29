60c2aefa42f1be05b8b5ebb533c591129dd0e7d0
Sara Freitas <sara.ft.freitas@hotmail.com>
2021-06-29 19:40:55 +0100

    tags creation structure

--- README.md
+++ README.md
@@ -10,11 +10,11 @@
 
 - Cadastro de TAG
 
- [ ] Não é permitido cadastrar mais de uma tag com o mesmo nome
+ [X] Não é permitido cadastrar mais de uma tag com o mesmo nome
 
- [ ] Não é permitido cadastrar tag sem nome
+ [X] Não é permitido cadastrar tag sem nome
 
- [ ] Não é permitido o cadastro por usuarios que nao sejam administradores 
+ [X] Não é permitido o cadastro por usuarios que nao sejam administradores 
 
 - Cadastro de elogio 
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { createTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 const createUserController = new CreateUserController();
+const creatTagController = new createTagController();
 
 router.post("/users", createUserController.handle )
+router.post("/tags", ensureAdmin , creatTagController.handle )
+
+
+
 
 export {router}
