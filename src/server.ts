1d3b0e03d1e2d25a4b181a462d1473e24ebca1a7
Sara Freitas <sara.ft.freitas@hotmail.com>
2021-07-01 23:12:03 +0100

    import cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import express, {Request, Response, NextFunction} from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import "./database";
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
